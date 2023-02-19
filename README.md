cefd059cec4f6d6070e127c26e4f9042f544a267
j-hil <92876062+j-hil@users.noreply.github.com>
2023-02-19 18:19:27 +0000

    Improve `depgraph`
    Add logging, types and docs. Remove `eval` usage.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # J-Tools. Some utility code that I've written.
+
+A dumping ground for python code that I've thrown together and think I might use again.
+
+None of this should be relied upon.
--- src/jtools/depgraph.py
+++ src/jtools/depgraph.py
@@ -1,15 +1,31 @@
+r"""Tools to generate dependency graphs for python packages.
+
+Use `write_dot_depgraph` to create a DOT file to render/view,                 AAA
+or use `mk_depgraph` to return the equivalent `networkx.DepGraph`            /   \
+object for further manipulation.                                           BBB    CCC
+                                                                            |    /   \
+The information displayed is intentionally minimal, as the primary         DDD  EEE  FFF
+use-case is visualizing the data; only inputted packages are displayed
+as nodes and the information is transitively reduced.
+"""
+
 from __future__ import annotations
 
+import ast
+import logging
 import subprocess
 import tempfile
 import venv
 from functools import lru_cache
 from pathlib import Path
-from typing import TextIO
+from typing import Iterable
 
 import networkx as nx
 from packaging import markers
 from packaging.requirements import Requirement
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 _DOT_TEMPLATE = """\
 // generated using `jtools.depgraph`
@@ -33,29 +49,34 @@ def _eval_marker(r: Requirement) -> bool:
 
 
 def _requires(pkg: str, py_exe: Path) -> list[str]:
+    """Runs `importlib.metadata.requires` but in environment specified by `py_exe`."""
     result_repr = subprocess.run(
-        [py_exe, "-c", _REQUIRES_SCRIPT_TEMPLATE.format(pkg)], capture_output=True
+        [py_exe, "-c", _REQUIRES_SCRIPT_TEMPLATE.format(pkg)],
+        capture_output=True,
+        check=True,
     ).stdout.decode()
-    # TODO: find a better way to do this?
-    return eval(result_repr) or []  # oh dear...
+    return ast.literal_eval(result_repr) or []
 
 
 @lru_cache(1000)
-def _requirements_and_edges(pkg: str, py_exe):
+def _requirements_and_edges(
+    pkg: str, py_exe: Path
+) -> tuple[list[str], list[tuple[str, str]]]:
     reqs = [Requirement(req_str) for req_str in _requires(pkg, py_exe)]
-    # TODO: duplicated code here:
-    return (r.name for r in reqs if _eval_marker(r)), (
-        (pkg, r.name) for r in reqs if _eval_marker(r)
-    )
+    req_strs = [r.name for r in reqs if _eval_marker(r)]
+    return req_strs, [(pkg, r) for r in req_strs]
 
 
-def _get_edges(pkgs, py_exe):
+def _get_edges(pkgs: Iterable[str], py_exe: Path) -> set[tuple[str, str]]:
     if not pkgs:
-        return []
+        return set()
+
     edges = set()
     next_pkgs = set()
     for pkg in pkgs:
         more_pkgs, more_edges = _requirements_and_edges(pkg, py_exe)
+        logger.debug("Package %s upstream dependencies acquired as: %s", pkg, more_pkgs)
+
         edges.update(more_edges)
         next_pkgs.update(more_pkgs)
     edges.update(_get_edges(next_pkgs, py_exe))
@@ -77,30 +98,34 @@ def mk_depgraph(pkgs: list[str], py_exe: Path | None = None) -> nx.DiGraph:
         with tempfile.TemporaryDirectory() as tempdir_name:
             dep_venv = Path(tempdir_name)
             venv.create(dep_venv, with_pip=True)
-            print("venv created at ", dep_venv)
+            logger.info("No python executable specified, venv created at %s", dep_venv)
             return mk_depgraph(pkgs, dep_venv / "Scripts" / "python.exe")
 
+    logger.info("Installing packages with %s", py_exe)
     p = subprocess.run(
-        [str(py_exe), "-m", "pip", "install", *pkgs], capture_output=True
+        [str(py_exe), "-m", "pip", "install", *pkgs], capture_output=True, check=False
     )
     if p.returncode:
         raise RuntimeError(f"Installing {pkgs=} failed. Error:\n\n{p.stderr.decode()}")
 
+    logger.info("Packages installed. Creating network graph.")
     g = nx.DiGraph()
     g.add_edges_from(_get_edges(pkgs, py_exe))
+
+    logger.info("Reducing network graph.")
     g = nx.transitive_closure(g)
     g.remove_nodes_from(node for node in list(g.nodes) if node not in pkgs)
     return nx.transitive_reduction(g)
 
 
-def to_dot(g: nx.DiGraph, fp: TextIO | None = None) -> str:
+def to_dot(g: nx.DiGraph) -> str:
     """Naively convert a directed graph into DOT format & optionally write to file.
 
-    DOT language described here: https://graphviz.org/doc/info/lang.html
+    DOT language described here: https://graphviz.org/doc/info/lang.html. Note Will set
+    `rankdir=RL` since it makes nicer graphs when rendered.
 
     :param g: Directed Graph to convert.
-    :param fp: Optional file to write DOT to. Will set `rankdir=RL`.
-    :return: Graph `g` in DOT format
+    :return: Graph `g` in DOT format as a string
     """
     # Why not use `pydot` or `pygraphviz`?
     #
@@ -111,44 +136,24 @@ def to_dot(g: nx.DiGraph, fp: TextIO | None = None) -> str:
     #   making the output deterministic and minimizing diffs as graphs are re-generated.
     str_nodes = "".join(f'    "{n}";\n' for n in sorted(g.nodes))
     str_edges = "".join(f'    "{a}" -> "{b}";\n' for a, b in sorted(g.edges))
-    result = _DOT_TEMPLATE.format(str_nodes + str_edges)
-    if fp:
-        fp.write(result)
-    return result
+    return _DOT_TEMPLATE.format(str_nodes + str_edges)
 
 
-def main():
-    pkgs = [
-        "awscli",
-        "pandas",
-        "tensorflow",
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "keras",
-        "torch",
-        "scrapy",
-        "beautifulsoup4",  # module: bs4
-        "typing-extensions",
-        "pydot",
-        "networkx",
-        "black",
-        "pylint",
-        "mypy",
-        "boto3",
-        "urllib3",
-        "setuptools",
-        "requests",
-        "botocore",
-        "idna",
-        "charset-normalizer",
-        "wheel",
-    ]
-
+def write_dot_depgraph(pkgs: list[str], path: Path) -> None:
     g = mk_depgraph(pkgs)
-    with open("./temp.gv", "w") as fp:
-        print(to_dot(g, fp))
+    path.write_text(to_dot(g))
+    logger.info("Result written to file %s", path)
+
+
+def _main() -> None:
+    # test example
+    pkgs = (
+        "awscli pandas tensorflow numpy scipy matplotlib keras torch scrapy "
+        "beautifulsoup4 typing-extensions pydot networkx black pylint mypy boto3 "
+        "urllib3 setuptools requests botocore idna charset-normalizer wheel"
+    ).split()
+    write_dot_depgraph(pkgs, Path.cwd() / "temp.gv")
 
 
 if __name__ == "__main__":
-    main()
+    _main()
